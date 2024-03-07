a5f4ef04ca665fcbe89641f254b6545b49ee5e38
sammyniyo <samshakul@gmail.com>
2024-03-07 13:28:43 +0200

    new fixes

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const passwordBox = document.getElementById("password");
 const length = 12;
 
-const upperCase = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
-const lowerCase =  "abcdefghijklmnoprstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "1234567890";
 const symbol = "@#$%^&*()_+~|}{[]></-=";
 
 const allChars = upperCase + lowerCase + number + symbol;
 
-function createPassword(){
-    let password = "";
-        password += upperCase[Math.floor(Math.random() * upperCase.length)];
-        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-        password += number[Math.floor(Math.random() * number.length)];
-        password += symbol[Math.floor(Math.random() * symbol.length)];
+function createPassword() {
+    let password = '';
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
 
-        while(length > password.length){
-            password += allChars[Math.floor(Math.random * symbol.length)];
+    while (password.length < length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
 
-        }
+    // Shuffle the password
+    password = password.split('').sort(function() {
+        return 0.5 - Math.random();
+    }).join('');
+
     passwordBox.value = password;
 }
